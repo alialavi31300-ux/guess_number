900d38e73a80d22174b628622cd2ea53fffc1685
Ali <ali.alavi31300@gmail.com>
2025-12-10 16:26:32 +0330

    solve bug

--- main.py
+++ main.py
@@ -7,22 +7,33 @@ print("\n")
 print("I'm thinking of a number between 1 and 100." +"\n"
       "Can you guess it? " + "\n")
 
-user_number = int(input("Enter your guess: "))
 py_number = random.randint(1, 100)
 attempts = 10
+user_attempts = 1
+
+user_number = int(input(f"Attempt {user_attempts}/10" " - " "Enter your guess: "))
 
 while True:
-    if user_number == py_number:
-        print(f"Congratulations! You guessed it in {attempts} attempts!The number was {py_number}")
-        break
+        if user_number == py_number:
+            print("\n")
+            print(f"Congratulations! You guessed it in {user_attempts} attempts!The number was {py_number}")
+            break
 
-    elif user_number < py_number:
-        print("Go higher \u2B06\uFE0F")
-        user_number = int(input("Enter your guess: "))
-        print("\n")
-        attempts -= 1
-    else:
-        print("Go lower \u2B07\uFE0F")
-        user_number = int(input("Enter your guess: "))
-        print("\n")
-        attempts -= 1
+        elif user_number < py_number:
+            print("\n")
+            print("Go higher \u2B06\uFE0F")
+
+        else:
+            print("\n")
+            print("Go lower \u2B07\uFE0F")
+
+        user_attempts += 1
+
+        if user_attempts > 10:
+            print("\n")
+            print("Game over! You ran out of attempts!" + "\n" + f" The number was {py_number}")
+            break
+
+        user_number = int(input(f"Attempt {user_attempts}/{attempts} - Enter your guess: "))
+
+
